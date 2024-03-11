1580f9d53ae40fd06f1d4452175467d8d59d11fa
KamilGradzik <k.gradzik97@gmail.com>
2024-03-11 12:58:13 +0100

    Set up project and folder structure

---
+++ README.md
@@ -0,0 +1,1 @@
+# InvoiceWizard App
---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React, { ReactElement } from 'react';
+import logo from './assets/images/logo.svg';
+
+const App:React.FC = ():ReactElement => {
+  return (
+    <React.Fragment>
+      <h1>App WiP</h1>
+      <img src={logo}></img>
+    </React.Fragment>
+  )
+}
+
+export default App;
